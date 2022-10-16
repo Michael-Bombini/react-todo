62ff61690e101e5e91379d6af3f54b6a0b9d9040
Michael Bombini <bombini.michael@gmail.com>
2022-10-16 18:33:14 +0200

    Fixed when then todo not updating done marked

--- src/components/Todo/TodoElement.js
+++ src/components/Todo/TodoElement.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import './TodoElement.css';
 
 const TodoElement = (props) => {
-  const [theTodo, setTheTodo] = useState(props.todo);
   
   const todoDoneHandle = () => {
-    setTheTodo({ ...theTodo, done: !theTodo.done });
+    props.onMarkAsDone(props.index);
   };
   const todoDeleteHandle = () => {
     console.log(props.index);
     props.onDeleteTodo(props.index);
   }
   return (
-    <div className={theTodo.done ? "done" : ""}>
-      {theTodo.text} {theTodo.done}{" "}
+    <div className={props.todo.done ? "done" : ""}>
+      {props.todo.text} {props.todo.done}{" "}
       <button onClick={todoDoneHandle}>Mark as done</button>
       <button onClick={todoDeleteHandle}>Delete</button>
     </div>
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,39 +1,42 @@
 import React, { useState } from "react";
 import AddTodo from "../Todo/AddTodo";
 import TodoElement from "../Todo/TodoElement";
-import './Card.css';
-
+import "./Card.css";
 
 const Card = () => {
-  const [todo,setTodo] = useState([]);
-  
+  const [todo, setTodo] = useState([]);
+
   const addTodo = (newTodo) => {
     console.log("arrivato in card");
     console.log(newTodo);
-    
-    setTodo((currentTodos) =>{
 
-      return [...currentTodos , newTodo ];
+    setTodo((currentTodos) => {
+      return [...currentTodos, newTodo];
     });
   };
-
-  // const markAsDone = ind => {
-  //   const newTodos = [...todo].filter((elem,index) => index !== ind);
-  //   setTodo(newTodos);
-  // }
-
-  const deleteTodo = ind => {
-    const newTodos = [...todo].filter((elem,index) => index !== ind);
-    setTodo(newTodos);
-  }
-
   
+  const markAsDone = (ind) => {
+    const newTodos = [...todo];
+    newTodos[ind].done = true;
+    setTodo(newTodos);
+  };
+
+  const deleteTodo = (ind) => {
+    const newTodos = [...todo].filter((elem, index) => index !== ind);
+    setTodo(newTodos);
+  };
 
   return (
     <div className="todo__card">
-      <AddTodo onAddTodo={addTodo}/>
-      {todo.map((el,index) => (
-        <TodoElement key={el.text + index} todo={el} index={index} onDeleteTodo={deleteTodo}/>
+      <AddTodo onAddTodo={addTodo} />
+      {todo.map((el, index) => (
+        <TodoElement
+          key={el.text + index}
+          todo={el}
+          index={index}
+          onDeleteTodo={deleteTodo}
+          onMarkAsDone={markAsDone}
+        />
       ))}
     </div>
   );
